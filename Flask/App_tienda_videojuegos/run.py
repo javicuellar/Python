fac564adebc02201bd8bee9ed0d1b0d0589edf7d
javicuellar <javicu25@gmail.com>
2025-04-21 21:25:06 +0200

    Revert "App web Tienda videojuegos (Python Flask)"
    This reverts commit 57f3dfee93bbbb9e7f946e249c0a7c7372eb3dce.

--- Flask/App_tienda_videojuegos/run.py
+++
@@ -1,8 +0,0 @@
-# import sys
-# sys.path.insert(0, '/var/www/html/tienda_videojuegos')
-
-from aplicacion.app import app as application
-
-
-if __name__ == "__main__":
-    application.run(port=5004)
--- Flask/App_tienda_videojuegos/tienda_videojuegos-docker/app.wsgi
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/html/tienda_videojuegos')
-
-from aplicacion.app import app as application
-
